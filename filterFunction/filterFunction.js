23fae8bb984b8283d9efb65d347939d982d088e1
Nicole Martin <n.martinpdx@gmail.com>
2021-03-26 13:31:25 -0700

    myFilter test pass

--- filterFunction/filterFunction.js
+++ filterFunction/filterFunction.js
@@ -1,3 +1,9 @@
-const myFilter = (arr, cb) => {};
+const myFilter = (arr, cb) => {
+  let filterArr = [];
+  for (let array of arr) {
+    if (cb(array)) filterArr = [...filterArr, array];
+  }
+  return filterArr;
+};
 
 module.exports = myFilter;
--- filterFunction/filterFunction.test.js
+++ filterFunction/filterFunction.test.js
@@ -1,5 +1,10 @@
-const myFilter = require('./mapFunction');
+const myFilter = require('./filterFunction');
 
 describe('filter function', () => {
-  it('', () => {});
+  it('Takes an array and callback, creates a new returned true', () => {
+    const numbers = [2, 3, 4, 6, 10, 33, 54];
+    const cb = (nums) => nums >= 5;
+
+    expect(myFilter(numbers, cb)).toEqual([6, 10, 33, 54]);
+  });
 });
